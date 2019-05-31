05268263e6beb5e3fa78ecd09510e4fab5e1df77
Nicke Yu <niyu@bender.engr.ucr.edu>
2019-05-30 17:13:13 -0700

    Comments Guideline

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import forward_prop
+import backward_prop
+import tensorflow as tf
+
+
+def main():
+
+    input = "abcdefghijklmnop" #input string - can be anything for encoding
+
+    #call backward_prop - input is alphabet of strings; return weights (encode) 
+    #call forward_prop function - returns output (encode)
+    #print encoded message
+
+    #call backward_Prop - input is output of forward_prop of encoding; returns weights (decode)
+    #call forward_prop - returns output (decode) 
+
+    #print decoded message statements
+
 
