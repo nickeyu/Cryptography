5e867b33b81acc26a6dc4a9cc584a5200e270faf
Jesus Reynoso <jereynoso@bender.engr.ucr.edu>
2019-06-03 20:54:40 -0700

    turned into function

--- backward_prop.py
+++ backward_prop.py
@@ -1,71 +1,79 @@
 import tensorflow as tf
 
-# Define parameters
-learning_rate = 0.004
-num_epochs = 3000
-N = 26  # Number of data
-width = 5  # Data width
-inputs = []
-for i in range(N):
-    temp = []
-    curVal = i
-    for j in range(width):
-        temp.append(curVal % 2)
-        curVal = curVal / 2
-    temp.reverse()
-    inputs.append(temp)
+def Backward_Propogation(inputs, outputs, N, width):
+    # Define parameters
+    learning_rate = 0.004
+    num_epochs = 3000
 
-outputs = []
-for i in range(1, N + 1):
-    outputs.append(i / float(N))
+    # Build the graph
+    tf.reset_default_graph()
+    X = tf.placeholder(dtype=tf.float32)
 
-# Build the graph
-tf.reset_default_graph()
-X = tf.placeholder(dtype=tf.float32)
+    W = tf.Variable(
+        tf.random_normal(shape=[width, width],
+                        mean=0.5, stddev=1.0, dtype=tf.float32
+                        ),
+        dtype=tf.float32
+        )
 
-W = tf.Variable(
-    tf.random_normal(shape=[width, width],
-                     mean=0.5, stddev=1.0, dtype=tf.float32
-                     ),
-    dtype=tf.float32
-    )
+    B = tf.Variable(
+        tf.zeros(shape=[width, 1], dtype=tf.float32),
+        )
 
-B = tf.Variable(
-    tf.zeros(shape=[width, 1], dtype=tf.float32),
-    )
+    # Without activation function
+    # Z = tf.reduce_sum(W * A + B)
+    # With activation function
+    Z = tf.reduce_sum(tf.tanh(W * X + B))
 
-# Without activation function
-# Z = tf.reduce_sum(W * A + B)
-# With activation function
-Z = tf.reduce_sum(tf.tanh(W * X + B))
+    init = tf.global_variables_initializer()
 
-init = tf.global_variables_initializer()
+    # Loss & Optimizer
+    Y = tf.placeholder(dtype=tf.float32)
+    loss = tf.losses.mean_squared_error(Y, Z)
+    optimizer = tf.train.GradientDescentOptimizer(
+        learning_rate=learning_rate).minimize(loss)
 
-# Loss & Optimizer
-Y = tf.placeholder(dtype=tf.float32)
-loss = tf.losses.mean_squared_error(Y, Z)
-optimizer = tf.train.GradientDescentOptimizer(
-    learning_rate=learning_rate).minimize(loss)
+    # Define and run the session
+    with tf.Session() as sess:
+        sess.run(init)
+        for epoch in range(num_epochs):
+            total_loss = 0
+            print("\nEpoch {}/{}".format(epoch + 1, num_epochs))
+            for i in range(N):
+                _, out, out_loss = sess.run(
+                    [optimizer, Z, loss],
+                    feed_dict={X: inputs[i], Y: outputs[i]}
+                    )
+                total_loss += out_loss
+                print("Entry {}: {:0.4f} (Expected: {:0.4f})".format(i, out, outputs[i]))
+        #      print(out)
+        #      print(outputs[i])
+            weights = sess.run(W)
+            labels = sess.run(B) 
+            print("Total loss: {:0.4f}".format(total_loss))
+        print(labels)
 
-# Define and run the session
-with tf.Session() as sess:
-  sess.run(init)
-  for epoch in range(num_epochs):
-    total_loss = 0
-    print("\nEpoch {}/{}".format(epoch + 1, num_epochs))
+    return weights
+
+
+def main():
+    N = 26  # Number of data
+    width = 5  # Data width
+    inputs = []
     for i in range(N):
-      _, out, out_loss = sess.run(
-          [optimizer, Z, loss],
-          feed_dict={X: inputs[i], Y: outputs[i]}
-          )
-      total_loss += out_loss
-      print("Entry {}:  (Expected: ".format(i))
-      print(out)
-      print(outputs[i])
-      weights = sess.run(W)
-      labels = sess.run(B) 
-    print("Total loss: {:0.4f}".format(total_loss))
-  print(weights)
-  print(labels)
+        temp = []
+        curVal = i
+        for j in range(width):
+            temp.append(curVal % 2)
+            curVal = curVal / 2
+        temp.reverse()
+        inputs.append(temp)
 
+    outputs = []
+    for i in range(1, N + 1):
+        outputs.append(i / float(N))
+    outputs.reverse()
+    back_weights = Backward_Propogation(inputs, outputs, N, width)
+    print(back_weights)
 
+main()
